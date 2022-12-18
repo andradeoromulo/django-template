5f6778e7834ab449432c39f0dc77328080308f1c
Rômulo Andrade <andrade.o.romulo@gmail.com>
2022-12-18 11:50:33 -0300

    Add image field to Product

--- product/models.py
+++ product/models.py
@@ -5,4 +5,5 @@ class Product(models.Model):
     code = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
+    image = models.ImageField(upload_to='products/')
     created_at = models.DateTimeField(default=datetime.now)
